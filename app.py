97581a369dcb6b03546fb4f5e43ad17effa6f004
Richym79 <43337187+Richym79@users.noreply.github.com>
2018-11-24 17:01:38 +0000

    Updated routing for all pages

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def root():
 	story_data = []
-	chapter_title = "";
 	with app.open_resource('static/json/story_content.json') as stories:
 		story_array = json.load(stories)
 		total_stories = len(story_array)
@@ -20,13 +19,120 @@ def chapter1():
 	story_data = []
 	with app.open_resource('static/json/story_content.json') as stories:
 		story_array = json.load(stories)
-		for obj in story_array:
-			chapter = str(obj["chapter"])
-			if chapter == "1":
-				story_data.append(obj)
+		total_stories = len(story_array)
+		for story in story_array:
+			story_chapter = str(story['chapter'])
+			if story_chapter == "1":
+				story_data.append(story)	
+				
+	return render_template('index.html', story_data=story_data)
 
-	return render_template('index.html', story=story_data)
-  
+@app.route('/chapter2/')
+def chapter2():
+	story_data = []
+	with app.open_resource('static/json/story_content.json') as stories:
+		story_array = json.load(stories)
+		total_stories = len(story_array)
+		for story in story_array:
+			story_chapter = str(story['chapter'])
+			if story_chapter == "2":
+				story_data.append(story)	
+				
+	return render_template('index.html', story_data=story_data)
+
+@app.route('/chapter3/')
+def chapter3():
+	story_data = []
+	with app.open_resource('static/json/story_content.json') as stories:
+		story_array = json.load(stories)
+		total_stories = len(story_array)
+		for story in story_array:
+			story_chapter = str(story['chapter'])
+			if story_chapter == "3":
+				story_data.append(story)	
+				
+	return render_template('index.html', story_data=story_data)
+
+@app.route('/chapter4/')
+def chapter4():
+	story_data = []
+	with app.open_resource('static/json/story_content.json') as stories:
+		story_array = json.load(stories)
+		total_stories = len(story_array)
+		for story in story_array:
+			story_chapter = str(story['chapter'])
+			if story_chapter == "4":
+				story_data.append(story)	
+				
+	return render_template('index.html', story_data=story_data)
+
+@app.route('/chapter5/')
+def chapter5():
+	story_data = []
+	with app.open_resource('static/json/story_content.json') as stories:
+		story_array = json.load(stories)
+		total_stories = len(story_array)
+		for story in story_array:
+			story_chapter = str(story['chapter'])
+			if story_chapter == "5":
+				story_data.append(story)	
+				
+	return render_template('index.html', story_data=story_data)
+    
+@app.route('/chapter6/')
+def chapter6():
+	story_data = []
+	with app.open_resource('static/json/story_content.json') as stories:
+		story_array = json.load(stories)
+		total_stories = len(story_array)
+		for story in story_array:
+			story_chapter = str(story['chapter'])
+			if story_chapter == "6":
+				story_data.append(story)	
+				
+	return render_template('index.html', story_data=story_data)
+
+@app.route('/chapter7/')
+def chapter7():
+	story_data = []
+	with app.open_resource('static/json/story_content.json') as stories:
+		story_array = json.load(stories)
+		total_stories = len(story_array)
+		for story in story_array:
+			story_chapter = str(story['chapter'])
+			if story_chapter == "7":
+				story_data.append(story)	
+				
+	return render_template('index.html', story_data=story_data)
+
+@app.route('/chapter8/')
+def chapter8():
+	story_data = []
+	with app.open_resource('static/json/story_content.json') as stories:
+		story_array = json.load(stories)
+		total_stories = len(story_array)
+		for story in story_array:
+			story_chapter = str(story['chapter'])
+			if story_chapter == "8":
+				story_data.append(story)	
+				
+	return render_template('index.html', story_data=story_data)
+
+@app.route('/outcome1/')
+def outcome1():
+	return render_template('index.html', outcome_selected="1")
+
+@app.route('/outcome2/')
+def outcome1():
+	return render_template('index.html', outcome_selected="2")
+
+@app.route('/outcome3/')
+def outcome1():
+	return render_template('index.html', outcome_selected="3")
+
+@app.route('/outcome4/')
+def outcome1():
+	return render_template('index.html', outcome_selected="4")
 
 if __name__ == '__main__':
 	app.run(debug-True)
