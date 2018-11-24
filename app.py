11f8719e4284e3970a3d062419864709dcfa66d1
Richard McMaster <40280868@napier.ac.uk>
2018-11-24 16:20:23 +0000

    update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ def root():
 	story_data = []
 	with app.open_resource('static/json/story_content.json') as stories:
 		story_array = json.load(stories)
-		for obj in story_array:
-			chapter = str(obj["chapter"])
-			if chapter == "0":
-				story_data.append(obj)	
-				
-	return render_template('index.html', story=story_data)
+		total_stories = len(story_array)
+		for story in story_array:
+			story_data.append(story)	
+	
+	story_arr_length = len(story_data)			
+	return render_template('index.html', stories=total_stories, arr_len=story_arr_length, story=story_data)
 
 @app.route('/chapter1/')
 def chapter1():
